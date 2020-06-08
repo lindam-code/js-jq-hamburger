24a3b981e9f15c15f5dfb44e1a8eb60b2ec4d3d6
lindam <linda.minotti2@gmail.com>
2020-06-08 15:18:36 +0200

    Burgher menu con JQuery usando una classe già presente nel css

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-// Quando clicco sull'icona del burgher menu rendo visibile la lista dei link
+// // Usando hide e show
+//
+// // Quando clicco sull'icona del burgher menu rendo visibile la lista dei link
+// $('.header-right i').click(
+//   function() {
+//     $('.hamburger-menu').show();
+//   }
+// )
+//
+// // Quando clicco sull'icona della X, nascondo la lista dei link
+// $('.hamburger-menu .close').click(
+//   function(){
+//     $('.hamburger-menu').hide();
+//   }
+// )
+
+// Usando la classe active già scritta in css
+// Quando clicco sull'icona del burgher menu aggiungo la classe active
+// già presente in css che da display: block.
 $('.header-right i').click(
   function() {
-    $('.hamburger-menu').show();
+    $('.hamburger-menu').addClass('active');
   }
 )
 
-// Quando clicco sull'icona della X, nascondo la lista dei link
+// Quando clicco sull'icona della X, rimuovo la classe active
+// di conseguenza torna display: none
 $('.hamburger-menu .close').click(
-  function(){
-    $('.hamburger-menu').hide();
+  function() {
+    $('.hamburger-menu').removeClass('active');
   }
 )
